4ce3538d4556520d6541feeca6ea2d42902eb9d6
Marc <102824384+EvoX7@users.noreply.github.com>
2022-11-16 18:59:29 +0100

    fix: snack 7 fixed syntax

--- src/org/snacks/java/Snack7.java
+++ src/org/snacks/java/Snack7.java
@@ -6,23 +6,11 @@ public class Snack7 {
 
 		String[] words = { "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipisicing", "elit" };
 
-		String newString = words[0] + " "
-				.concat(words[1])
-				.concat(words[3] + " "
-			    .concat(words[4] + " "
-				.concat(words[5] + " "
-			    .concat(words[6] + " "
-				.concat(words[7])))));
-		
-		String newString2 = words[0] + " "
-				.concat(words[1])
-				.concat(words[3] + " "
-			    .concat(words[4] + " "
-				.concat(words[5] + " "
-			    .concat(words[6] + " "
-				.concat(words[7])))));
-		
-		System.out.println(newString);
+		for (int i = 0; i < words.length; i++) {
+
+			System.out.print(words[i] + " ");
+
+		}
 
 	}
 }
